c7ef50d983a751c3628304831ce23430da1a967b
ncrypted | Oliver <15632467+ncryptedV1@users.noreply.github.com>
2025-04-12 20:13:44 +0200

    edit: trt_dtype now configurable

--- runtime/triton_trtllm/run.sh
+++ runtime/triton_trtllm/run.sh
@@ -3,10 +3,10 @@ export CUDA_VISIBLE_DEVICES=0
 stage=$1
 stop_stage=$2
 service_type=$3
+trt_dtype=${4:-bfloat16}  # Default to bfloat16 if not provided
 echo "Start stage: $stage, Stop stage: $stop_stage service_type: $service_type"
 
 huggingface_model_local_dir=../../pretrained_models/Spark-TTS-0.5B
-trt_dtype=bfloat16
 trt_weights_dir=./tllm_checkpoint_${trt_dtype}
 trt_engines_dir=./trt_engines_${trt_dtype}
 
